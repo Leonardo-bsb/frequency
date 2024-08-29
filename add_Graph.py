13625ef8acc6d7131440f392fd09eb0906f1c78a
Leonardo Lisita <leo.lisita@hotmail.com>
2024-08-29 17:31:25 -0300

    working on graph

--- add_Graph.py
+++ add_Graph.py
@@ -11,5 +11,11 @@ print(blank_image)
 
 for i in range (height-1):
     for j in range(width-1):
-        print(str(i)+'_'+str(j))
+        print(str(i)+'_'+str(j),blank_image[i,j],'--',
+              str(i)+'_'+str(j+1),blank_image[i,j+1])
+        print(str(i)+'_'+str(j),blank_image[i,j],'--',
+              str(i+1)+'_'+str(j),blank_image[i+1,j])
+        print(str(i)+'_'+str(j),blank_image[i,j],'--',
+              str(i+1)+'_'+str(j+1),blank_image[i+1,j+1])
+        
 
