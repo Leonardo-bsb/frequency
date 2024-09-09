f40d6a59ce1f3b02ceaa10cc72e932e0238f75d2
Leonardo Lisita <leo.lisita@hotmail.com>
2024-09-09 19:13:28 -0300

    refatorando o código

--- gray_scale_image_with_graph_3_characters.py
+++ gray_scale_image_with_graph_3_characters.py
@@ -11,24 +11,9 @@ def generate_text_image(height,width,text):
     fontScale = 3
     color = (0)
     thickness = 5
-    # image = cv2.putText(blank_image, 'a b', org, font, 
-    #                 fontScale, color, thickness, cv2.LINE_AA)
     image = cv2.putText(blank_image, text, org, font, 
                     fontScale, color, thickness, cv2.LINE_AA)
     return image
-
-# cv2.imshow('Image',blank_image)
-# cv2.waitKey(0)
-# cv2.destroyAllWindows()
-
-# print( "Image")
-# unique0, counts0 = np.unique(image, return_counts=True)
-# print(unique0,counts0)
-
-
-# plt.hist(blank_image,bins=30)
-# plt.show()
-
 
 import networkx as nx
 height,width=200,250
@@ -56,7 +41,6 @@ for i in range (height-1):
 print(G.number_of_nodes())
 print(G.number_of_edges())
 
-#print(list(G.edges))
 
 number_connected_components=nx.number_connected_components(G)
 print(number_connected_components)
@@ -74,30 +58,6 @@ connected=[c for c in sorted(nx.connected_components(G), key=len, reverse=True)]
 for i in range(100):
      print('Connected ', i, ':', len(connected[i]))
 
-
-# print(connected)
-
-# blank_image_1=grey_level* np.ones((height,width),dtype=np.uint8)
-
-# for edge in connected[0]:
-#     blank_image_1[edge[0],edge[1]]=0
-
-# for edge in connected[1]:
-#     blank_image_1[edge[0],edge[1]]=255
-
-
-# horizontal_image=np.concatenate((blank_image,blank_image_1),axis=1)
-# cv2.imshow('Horizontal Image',horizontal_image)
-# cv2.waitKey(0)
-# cv2.destroyAllWindows()
-
-# unique, counts = np.unique(blank_image, return_counts=True)
-# print( "Blank Image")
-# print(unique,counts)
-
-# print( "Blank Image 1")
-# unique1, counts1 = np.unique(blank_image_1, return_counts=True)
-# print(unique1,counts1)
 blank_image_BGR = np.zeros((height,width,3), np.uint8)
 for node in connected[0]:
     blank_image_BGR[node[0],node[1]]=(255,255,255)
@@ -114,9 +74,6 @@ for node in connected[3]:
 
 for node in connected[4]:
     blank_image_BGR[node[0],node[1]]=(0,255,255)
-
-
-
 
 
 cv2.imshow('Color Image',blank_image_BGR)
@@ -154,27 +111,6 @@ cv2.imshow('Image with Roi',image_roi)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
 
-# def generate_object_graph(image,nodes):
-#     rows=set()
-#     cols=set()
-#     step=8
-#     for node in nodes:
-#         rows.add(node[0])
-#         cols.add(node[1])
-#     x1=min(cols)
-#     x2=max(cols)
-#     y1=min(rows)
-#     y2=max(rows)
-#     
-#     vertices=np.zeros(shape=(step,step),dtype=np.uint8)
-#     i=0;j=0
-#     for row in y_points:
-#         for col in x_points:
-#             vertices[i,j]=image[row,col]
-#             j=j+1
-#         i=i+1
-#         j=0
-#     return vertices
 
 def generate_object_graph(image,nodes):
     points_width=16
@@ -196,16 +132,12 @@ def generate_object_graph(image,nodes):
     for node in nodes:
         object_matriz[node[0]-y1,node[1]-x1]=image[node[0],node[1]]
         
-    
-    # x_points=np.floor(np.linspace(x1,x2,step,endpoint=True)).astype(np.uint8)
-    # y_points=np.floor(np.linspace(y1,y2,step,endpoint=True)).astype(np.uint8)
+ 
     x_points=np.floor(np.linspace(0,width-1,points_width,endpoint=True)).astype(np.uint8)
     y_points=np.floor(np.linspace(0,height-1,points_height,endpoint=True)).astype(np.uint8)
     print('x_points size',np.size(x_points))
     print('y_points size',np.size(y_points))
 
-    # x_step=np.floor((x2-x1)/step).astype(np.uint8)
-    # y_step=np.floor((y2-y1)/step).astype(np.uint8)
     for i in range(points_height):
         for j in range(points_width):
             vertices[i,j]=object_matriz[y_points[i],x_points[j]]
@@ -213,10 +145,6 @@ def generate_object_graph(image,nodes):
     return vertices
     
 
-   
-        
-        
-    # return vertices
 
 vertices=generate_object_graph(image,connected[1])
 print(vertices)
